2482788994d4036c016e044b36156e805a907c80
djimi <djimi9530@gmail.com>
2023-11-10 17:34:01 +0100

    test script

--- scrape.py
+++ scrape.py
@@ -1,9 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 
-res = requests.get('https://www.scrapethissite.com/pages/')
+res = requests.get('https://www.imdb.com/search/title/?release_date=2023-01-01,2023-12-31')
 
 if res.ok:
+    i = 1
     soup = BeautifulSoup(res.text, 'html.parser')
-    title = soup.find('h1')
-    print(title.text)
+    movies = soup.find_all('li', ['ipc-metadata-list-summary-item sc-59b6048d-0 jemTre cli-parent'])
+    print(dir(movies))
+    for movie in movies:
+        movie_title = movie.findChildren('h3', 'ipc-title__text')
+        print(dir(movie))
+        i += 1
+        if i == 1:
+            break
+
+    # my_lis = soup.find_all('li')
+    # for li in my_lis:
+    #      doc = dir(li)
+    #      for d in doc:
+    #          print(d)
+    #         if li.attrs["class"] == ['ipc-inline-list__item']:
+    #             print(li.attrs)
+    #             label = li.attrs["class"]
+    #             print(label)
+
+    divs = soup.find_all('div')
+    for div in divs:
+        doc = dir(div)
+        for d in doc:
+            print(d)
