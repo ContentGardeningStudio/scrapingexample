476768d6d2bf46557d49af1da771f91776d34351
djimi <djimi9530@gmail.com>
2023-11-13 15:19:10 +0100

    dislpay all the movies name

--- scrape.py
+++ scrape.py
@@ -3,30 +3,22 @@ from bs4 import BeautifulSoup
 
 res = requests.get('https://www.imdb.com/search/title/?release_date=2023-01-01,2023-12-31')
 
-if res.ok:
-    i = 1
+if res.status_code == 200:
     soup = BeautifulSoup(res.text, 'html.parser')
-    movies = soup.find_all('li', ['ipc-metadata-list-summary-item sc-59b6048d-0 jemTre cli-parent'])
-    print(dir(movies))
+    # Affiche le 1er film
+    # print(soup.find('h3').findChild('a').text)
+    i = 1
+    movies = soup.find_all('h3', 'lister-item-header')
     for movie in movies:
-        movie_title = movie.findChildren('h3', 'ipc-title__text')
-        print(dir(movie))
+        movie_title = movie.find('a')
+        print(str(i) + '.' + movie_title.text)
         i += 1
-        if i == 1:
-            break
 
-    # my_lis = soup.find_all('li')
-    # for li in my_lis:
-    #      doc = dir(li)
-    #      for d in doc:
-    #          print(d)
-    #         if li.attrs["class"] == ['ipc-inline-list__item']:
+    # lis = soup.find_all('li')
+    # for li in lis:
+    #     for d in dir('li'):
+    #         print(d)
+    #         if li.attrs['class'] == ['ipc-inline-list__item']:
     #             print(li.attrs)
-    #             label = li.attrs["class"]
+    #             label = li.attrs['class']
     #             print(label)
-
-    divs = soup.find_all('div')
-    for div in divs:
-        doc = dir(div)
-        for d in doc:
-            print(d)
