51fd9a0776b49daffa98599ed15ad256c2d63034
kamon <kamon.ayeva@gmail.com>
2023-11-13 15:36:40 +0100

    Use enumerate() built-in function instead of maintaining the indice value ourselves

--- scrape.py
+++ scrape.py
@@ -1,18 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-res = requests.get('https://www.imdb.com/search/title/?release_date=2023-01-01,2023-12-31')
+URL = 'https://www.imdb.com/search/title/?release_date=2023-01-01,2023-12-31'
+res = requests.get(URL)
 
 if res.status_code == 200:
     soup = BeautifulSoup(res.text, 'html.parser')
     # Affiche le 1er film
     # print(soup.find('h3').findChild('a').text)
-    i = 1
+
     movies = soup.find_all('h3', 'lister-item-header')
-    for movie in movies:
+    for indice, movie in enumerate(movies, 1):
         movie_title = movie.find('a')
-        print(str(i) + '.' + movie_title.text)
-        i += 1
+        print(str(indice) + '.' + movie_title.text)
 
     # lis = soup.find_all('li')
     # for li in lis:
