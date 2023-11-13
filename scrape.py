68407473ace5da94fa504838d46a3b9c84c34fde
kamon <kamon.ayeva@gmail.com>
2023-11-13 16:02:20 +0100

    Change the target URL and the strategy in order to extract more data, not just the title

--- scrape.py
+++ scrape.py
@@ -1,7 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://www.imdb.com/search/title/?release_date=2023-01-01,2023-12-31'
+# URL = 'https://www.imdb.com/search/title/?release_date=2023-01-01,2023-12-31'
+
+# Search of films released since 01/01/2023
+URL = 'https://www.imdb.com/search/title/?release_date=2023-01-01,'
+
 res = requests.get(URL)
 
 if res.status_code == 200:
@@ -9,16 +13,17 @@ if res.status_code == 200:
     # Affiche le 1er film
     # print(soup.find('h3').findChild('a').text)
 
-    movies = soup.find_all('h3', 'lister-item-header')
-    for indice, movie in enumerate(movies, 1):
-        movie_title = movie.find('a')
-        print(str(indice) + '.' + movie_title.text)
+    movie_containers = soup.find_all("div", attrs={"class": "lister-item mode-advanced"})
+    print(movie_containers)
 
-    # lis = soup.find_all('li')
-    # for li in lis:
-    #     for d in dir('li'):
-    #         print(d)
-    #         if li.attrs['class'] == ['ipc-inline-list__item']:
-    #             print(li.attrs)
-    #             label = li.attrs['class']
-    #             print(label)
+    for mc in movie_containers:
+        # get all links and see if we can extract the title and other details from those links
+        links = mc.find_all("a")
+        print(links)
+        for link in links:
+            print(link.text, link.attrs["href"])
+
+    # movies = soup.find_all('h3', 'lister-item-header')
+    # for indice, movie in enumerate(movies, 1):
+    #     movie_title = movie.find('a')
+    #     print(str(indice) + '.' + movie_title.text)
