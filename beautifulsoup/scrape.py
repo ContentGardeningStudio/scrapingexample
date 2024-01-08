7050f5261304c39404c219870d34be099158679d
djimi <djimi9530@gmail.com>
2024-01-08 14:43:16 +0100

    test scraping title and url

--- beautifulsoup/scrape.py
+++ beautifulsoup/scrape.py
@@ -22,43 +22,56 @@ def scrape_url(website_url):
 
     # initiualize movies list
     movies = []
-
     if res.status_code == 200:
         soup = BeautifulSoup(res.text, 'html.parser')
+        # print(soup.prettify())
+        #class lien : ipc-lockup-overlay ipc-focusable
+        #class lien : ipc-title-link-wrapper
+        # metascore : sc-b0901df4-0 bcQdDJ metacritic-score-box
         # movie_containers = soup.find_all("div", attrs={"class": "lister-item mode-advanced"})
         movie_containers = soup.find_all("li", attrs={"class": "ipc-metadata-list-summary-item"})
 
-        print(movie_containers)
+        # print(movie_containers)
 
         for mc in movie_containers:
             # get all links and see if we can extract the title and other details from those links
             links = mc.find_all("a")
             # print(links)
-            title = ""
+            title = mc.find("h3")
+            metascore = mc.find("span", "sc-b0901df4-0 bcQdDJ metacritic-score-box")
             url = ""
-            participants = []
+            list_url = []
 
             # Init du dict de chaque movie
             movie = {}
 
-            for link in links[1:]:
-                href = link.attrs["href"]
-                text = link.text
-                if text != "":
-                    if href.startswith("/title/") and not href.endswith("/vote"):
-                        # print("Title ?", text, href)
-                        title = text
-                    elif href.startswith("/name/"):
-                        # print("Name ?", text, href)
-                        participants.append({"name": text, "href": href})
+            # for link in links[1:]:
+            #     href = link.attrs["href"]
+            #     text = link.text
+            #     if text != "":
+            #         if href.startswith("/title/") and not href.endswith("/vote"):
+            #             # print("Title ?", text, href)
+            #             title = text
+            #         elif href.startswith("/name/"):
+            #             # print("Name ?", text, href)
+            #             participants.append({"name": text, "href": href})
 
-            print(title, url, participants)
-            movie['title'] = title
-            movie['participants'] = participants
-            years = soup.find_all('span', attrs={"class": "lister-item-year text-muted unbold"})
-            for year in years:
-                movie['year'] = year.text
-                break
+            print("title : " + title.text)
+            print(links)
+            for link in links:
+                print(link.get('href'))
+                list_url.append(link.get('href'))
+            try:
+                print("metascore : " + metascore.text)
+            except:
+                pass
+            print('----------------------------------')
+            movie['title'] = title.text
+            # movie['participants'] = participants
+            # years = soup.find_all('span', attrs={"class": "lister-item-year text-muted unbold"})
+            # for year in years:
+            #     movie['year'] = year.text
+            #     break
             # print(movie)
 
             # Une fois le dict movie prêt, on l'ajoute à la liste des movies
@@ -67,9 +80,19 @@ def scrape_url(website_url):
     # the return needs to happen here
     return movies
 
+def print_html(web_url):
+    print("print html")
+    response = requests.get(web_url)
+    bs = BeautifulSoup(response.text, "html.parser")
+    print(response.status_code)
+    if response.status_code == 200:
+        print("ok")
+        print(bs.prettify())
+
 
 URL = 'https://www.imdb.com/search/title/?release_date=2023-01-01,'
-movies = scrape_url(URL)
-print(movies)
+scrape_url(URL)
+# movies = scrape_url(URL)
+# print(movies)
 # headers = ['title', 'url', 'participants', 'year']
 # save_data(headers, movies)
