196ee6ea905c4e9dda46c66a72f43a04b0df28ba
djimi <djimi9530@gmail.com>
2024-01-10 11:43:38 +0100

    final code scrape_url and save_data

--- beautifulsoup/scrape.py
+++ beautifulsoup/scrape.py
@@ -20,59 +20,43 @@ def save_data(headers, tab):
 def scrape_url(website_url):
     res = requests.get(website_url, headers=HEADERS)
 
-    # initiualize movies list
+    # initialize movies list
     movies = []
     if res.status_code == 200:
         soup = BeautifulSoup(res.text, 'html.parser')
         # print(soup.prettify())
-        #class lien : ipc-lockup-overlay ipc-focusable
-        #class lien : ipc-title-link-wrapper
-        # metascore : sc-b0901df4-0 bcQdDJ metacritic-score-box
         # movie_containers = soup.find_all("div", attrs={"class": "lister-item mode-advanced"})
         movie_containers = soup.find_all("li", attrs={"class": "ipc-metadata-list-summary-item"})
 
         # print(movie_containers)
 
         for mc in movie_containers:
+            BASE_URL = "https://imdb.com"
             # get all links and see if we can extract the title and other details from those links
             links = mc.find_all("a")
             # print(links)
             title = mc.find("h3")
             metascore = mc.find("span", "sc-b0901df4-0 bcQdDJ metacritic-score-box")
             url = ""
-            list_url = []
 
             # Init du dict de chaque movie
             movie = {}
 
-            # for link in links[1:]:
-            #     href = link.attrs["href"]
-            #     text = link.text
-            #     if text != "":
-            #         if href.startswith("/title/") and not href.endswith("/vote"):
-            #             # print("Title ?", text, href)
-            #             title = text
-            #         elif href.startswith("/name/"):
-            #             # print("Name ?", text, href)
-            #             participants.append({"name": text, "href": href})
-
-            print("title : " + title.text)
-            print(links)
+            # print("title : " + title.text)
+            # print(links)
             for link in links:
-                print(link.get('href'))
-                list_url.append(link.get('href'))
+                # print(link.get('href'))
+                if "sr_t" in link.get('href'):
+                    url += BASE_URL + link.get('href')
+            # print(url)
+            # print('----------------------------------')
+            movie['title'] = title.text
+            movie['href'] = url
             try:
-                print("metascore : " + metascore.text)
+                # print("metascore : " + metascore.text)
+                movie['metascore'] = metascore.text
             except:
                 pass
-            print('----------------------------------')
-            movie['title'] = title.text
-            # movie['participants'] = participants
-            # years = soup.find_all('span', attrs={"class": "lister-item-year text-muted unbold"})
-            # for year in years:
-            #     movie['year'] = year.text
-            #     break
-            # print(movie)
 
             # Une fois le dict movie prêt, on l'ajoute à la liste des movies
             movies.append(movie)
@@ -80,19 +64,9 @@ def scrape_url(website_url):
     # the return needs to happen here
     return movies
 
-def print_html(web_url):
-    print("print html")
-    response = requests.get(web_url)
-    bs = BeautifulSoup(response.text, "html.parser")
-    print(response.status_code)
-    if response.status_code == 200:
-        print("ok")
-        print(bs.prettify())
-
 
 URL = 'https://www.imdb.com/search/title/?release_date=2023-01-01,'
 scrape_url(URL)
-# movies = scrape_url(URL)
-# print(movies)
-# headers = ['title', 'url', 'participants', 'year']
-# save_data(headers, movies)
+list_movies = scrape_url(URL)
+headers = ['title', 'href', 'metascore']
+save_data(headers, list_movies)
